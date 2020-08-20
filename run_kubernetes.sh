8b199f944d0a0ba30f3fa6e2095b612583009657
mahdiuzzaman <mahdi.u.zaman@gmail.com>
2020-08-20 23:58:18 +0800

    add statements to run_kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="mahdiuzzaman/prediction-app"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deployment --image=$dockerpath prediction
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/prediction 8000:80
